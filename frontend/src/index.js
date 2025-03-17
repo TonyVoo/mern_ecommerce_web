500af6158619f927282fc11d17d9501d6fd4b928
Tvo <149646715+TonyVoo@users.noreply.github.com>
2025-03-17 11:25:31 +0700

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 import App from './App';
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
-const clientId = "549338674585-av5ja8h1j3jqd2rvljkegmdlhvu9o3tt.apps.googleusercontent.com";
+const clientId = "your_google_client";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
